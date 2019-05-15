d0706ad84a145041ce40ab2740e4626991353435
Janice Pena <jipena94@gmail.com>
2019-05-15 00:42:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+   delimiters = ['. ', "! ", "? "]
+   words = self.split(Regexp.union(delimiters))
+   counts = words.count
+   return counts
   end
 end
